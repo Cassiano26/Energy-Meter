fddb889cfd0567e9cd86b2c8fb0927885d2d4162
Cassiano26 <candidocassiano2@gmail.com>
2021-09-13 17:04:17 -0300

    noTax included, reset pzem included.

--- 3PhaseMeter.ino
+++ 3PhaseMeter.ino
@@ -7,9 +7,9 @@
 
 Preferences preferences;
 
-char auth[] = "KVCU50oyIiak_140XGZmLVOvWzrtpTeo";
-char ssid[] = "Cassiano2";
-char pass[] = "Cassiano9131";
+char auth[] = "********************************";
+char ssid[] = "************";
+char pass[] = "*******";
 
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = -14400;  // GM offset in seconds. this value can be a positive or negative number, depending on your location.
@@ -20,11 +20,16 @@ const int   daylightOffset_sec = 3600;
 int currentHour;
 int currentMinute;
 int currentWeekDay;
+bool wrongTime = true;
 
 float tax1;
 float tax2;
 float tax3;
-float lastEnergyValue;
+float noTax;
+float pzem1LastEnergyReading;
+float pzem2LastEnergyReading;
+float pzem3LastEnergyReading;
+// float lastEnergyValue;
 
 float voltage_phase01=0;
 float current_phase01=0;
@@ -61,8 +66,12 @@ void saveLocalTime(){
   struct tm timeinfo;
   if(!getLocalTime(&timeinfo)){
     Serial.println("Failed to obtain time");
+    wrongTime = true;
     return;
   }
+
+  wrongTime = false;
+   
   char hour[3];
   strftime(hour,3, "%H", &timeinfo);
   currentHour = String(hour).toInt(); 
@@ -198,65 +207,98 @@ void getData(){
   tax1 = preferences.getFloat("tax1", 0);
   tax2 = preferences.getFloat("tax2", 0);
   tax3 = preferences.getFloat("tax3", 0);
-  lastEnergyValue = preferences.getFloat("lastEnergyValue", 0);
+  noTax = preferences.getFloat("noTax", 0);
+  pzem1LastEnergyReading = preferences.getFloat("pzem1LastEnergyReading", 0);
+  pzem2LastEnergyReading = preferences.getFloat("pzem2LastEnergyReading", 0);
+  pzem3LastEnergyReading = preferences.getFloat("pzem3LastEnergyReading", 0);
+
+  //lastEnergyValue = preferences.getFloat("lastEnergyValue", 0);
 
   delay(1000);
   
 }
 
+void checkPzemResetEnergy() {
+  if (pzem1LastEnergyReading > energy_phase01) {
+    preferences.putFloat("pzem1LastEnergyReading", 0.0);
+  } 
+  
+  if(pzem2LastEnergyReading > energy_phase02){
+    preferences.putFloat("pzem2LastEnergyReading", 0.0);
+  }
+
+  if(pzem3LastEnergyReading > energy_phase03) {
+    preferences.putFloat("pzem3LastEnergyReading", 0.0)
+  }
+}
+
 void taxDivider(){
+
+  checkPzemResetEnergy();
   
   // tax
-  //     Saturday or Sunday
+  //     Saturday - 6. or Sunday - 7.
   //       tax1 06:00 to 21:30
   //       tax3 21:30 to 06:00
-  //     Other days
+  //     Other days - 1,2,3,4,5.
   //       tax1 06:00 to 17:30 and 20:30 to 21:30
   //       tax2 17:30 to 20:30
   //       tax3 21:30 to 06:00
 
-  float totalEnergy = energy_phase01 + energy_phase02 + energy_phase03;
-  float totalEnergyToSave = totalEnergy - lastEnergyValue;
+  
+  float totalEnergyPzem1 = energy_phase01 - pzem1LastEnergyReading;
+  float totalEnergyPzem2 = energy_phase02 - pzem2LastEnergyReading;
+  float totalEnergyPzem3 = energy_phase03 - pzem3LastEnergyReading;
 
-  if (currentWeekDay == 0 || currentWeekDay == 6 ) {
-    if(currentHour >= 6 && currentHour < 21) {
-      tax1 = tax1 + totalEnergyToSave;
-      preferences.putFloat("tax1", tax1);
-    } else if(currentHour == 21 && currentMinute < 30) {
-      tax1 = tax1 + totalEnergyToSave;
-      preferences.putFloat("tax1", tax1);
-    } else {
-      tax3 = tax3 + totalEnergyToSave;
-      preferences.putFloat("tax3", tax3);  
-    }
+  float totalEnergy = totalEnergyPzem1 + totalEnergyPzem2 + totalEnergyPzem3;
+
+  if(wrongTime){
+    noTax = noTax + totalEnergy;
+    preferences.putFloat("noTax", noTax);
   } else {
-    if (currentHour >= 6 && currentHour < 17 ){
-      tax1 = tax1 + totalEnergyToSave;
-      preferences.putFloat("tax1", tax1);
-    } else if(currentHour == 17 && currentMinute < 30) {
-      tax1 = tax1 + totalEnergyToSave;
-      preferences.putFloat("tax1", tax1);
-    } else if(currentHour == 17 && currentMinute >= 30) {
-      tax2 = tax2 + totalEnergyToSave;
-      preferences.putFloat("tax2", tax2);
-    } else if(currentHour == 18 || currentHour == 19) {
-      tax2 = tax2 + totalEnergyToSave;
-      preferences.putFloat("tax2", tax2);
-    } else if(currentHour == 20 && currentMinute < 30) {
-      tax2 = tax2 + totalEnergyToSave;
-      preferences.putFloat("tax2", tax2);
-    } else if(currentHour == 20 && currentMinute >= 30) {
-      tax1 = tax1 + totalEnergyToSave;
-      preferences.putFloat("tax1", tax1);
-    } else if(currentHour == 21 && currentMinute < 30) {
-      tax1 = tax1 + totalEnergyToSave;
-      preferences.putFloat("tax1", tax1);
+    if (currentWeekDay == 6 || currentWeekDay == 7 ) {
+      if(currentHour >= 6 && currentHour < 21) {
+        tax1 = tax1 + totalEnergy;
+        preferences.putFloat("tax1", tax1);
+      } else if(currentHour == 21 && currentMinute < 30) {
+        tax1 = tax1 + totalEnergy;
+        preferences.putFloat("tax1", tax1);
+      } else {
+        tax3 = tax3 + totalEnergy;
+        preferences.putFloat("tax3", tax3);  
+      }
     } else {
-      tax3 = tax3 + totalEnergyToSave;
-      preferences.putFloat("tax3", tax3);
-    }  
+      if (currentHour >= 6 && currentHour < 17 ){
+        tax1 = tax1 + totalEnergy;
+        preferences.putFloat("tax1", tax1);
+      } else if(currentHour == 17 && currentMinute < 30) {
+        tax1 = tax1 + totalEnergy;
+        preferences.putFloat("tax1", tax1);
+      } else if(currentHour == 17 && currentMinute >= 30) {
+        tax2 = tax2 + totalEnergy;
+        preferences.putFloat("tax2", tax2);
+      } else if(currentHour == 18 || currentHour == 19) {
+        tax2 = tax2 + totalEnergy;
+        preferences.putFloat("tax2", tax2);
+      } else if(currentHour == 20 && currentMinute < 30) {
+        tax2 = tax2 + totalEnergy;
+        preferences.putFloat("tax2", tax2);
+      } else if(currentHour == 20 && currentMinute >= 30) {
+        tax1 = tax1 + totalEnergy;
+        preferences.putFloat("tax1", tax1);
+      } else if(currentHour == 21 && currentMinute < 30) {
+        tax1 = tax1 + totalEnergy;
+        preferences.putFloat("tax1", tax1);
+      } else {
+        tax3 = tax3 + totalEnergy;
+        preferences.putFloat("tax3", tax3);
+      }  
     
+    }
+
   }
+
+  
 
   preferences.putFloat("lastEnergyValue", totalEnergy);
 
@@ -267,8 +309,8 @@ void taxDivider(){
 void myTimerEvent(){
 
   saveLocalTime(); 
-  readPzems();
   getData();
+  readPzems();
   taxDivider();
   
   // Sending data to blynk app.
@@ -291,6 +333,7 @@ void myTimerEvent(){
   Blynk.virtualWrite(V12, tax1);
   Blynk.virtualWrite(V13, tax2);
   Blynk.virtualWrite(V14, tax3);
+  Blynk.virtualWrite(V15, noTax);
   
 }
 
